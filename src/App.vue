753908edda74094ddf31def981518941153897a1
rafael-otto <rafael.august.otto.ifc@gmail.com>
2026-03-18 14:24:41 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <OfflineBanner />
+  <AppHeader />
+  <main>
+    <router-view />
+  </main>
+</template>
+
+<script setup>
+import AppHeader from './components/AppHeader.vue';
+import OfflineBanner from './components/OfflineBanner.vue';
+</script>
+
+<style scoped>
+main {
+  padding-bottom: 40px;
+}
+</style>
